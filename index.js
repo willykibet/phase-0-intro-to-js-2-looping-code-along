27a3e4627d6289272589818fff9572c68b6de537
Willy <willykipchumba22@gmail.com>
2023-09-21 16:18:12 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num) ;
+      num--;
+    }
+  }
+
+
+  function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+      cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+  }
